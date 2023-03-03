e6631b5da9237de67bf4be695036cb65ab64fcf3
Julian Jones <julianj96@yahoo.com>
2023-03-03 13:20:41 -0600

    coneecting the database

--- backend/config/db.config2.js
+++ backend/config/db.config2.js
@@ -3,12 +3,11 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
     'Grouponmania',
     'root',
-    'otakuJj9672$',{
+    'otakuJj9672$', {
         dialect: 'mysql',
         host: 'localhost',
-        port: '3306'
     }
 );
 
 
-module.exports = sequelize;
+module.exports = sequelize
