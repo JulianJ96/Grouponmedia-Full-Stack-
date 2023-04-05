c6bf668ef984ceb55fa61120c8c74b22f9c9affc
Julian Jones <julianj96@yahoo.com>
2023-04-05 16:16:22 -0500

    authentication and axios plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,11 @@
-import './plugins/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import axios from 'axios'
 import './assets/main.css'
-
+const base = axios.create({
+    baseURL: "http://localhost:3000/api"
+});
 const app = createApp(App)
 
 app.use(router)
