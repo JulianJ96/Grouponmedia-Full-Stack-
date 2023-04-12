7f70cdbc295242d6e2c3602457341a521449edef
Julian Jones <julianj96@yahoo.com>
2023-04-12 15:45:26 -0500

    adding images

---
+++ frontend/services/EventService.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export default{
+    async getEvents() {
+        let res = await axios.get("http://localhost:3000/api/auth");
+        return res.data;
+    },
+    async getEventSingle(id){
+        let res = await axios.get("http://localhost:")
+    }
+}
