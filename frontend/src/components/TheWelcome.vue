92459aa5cc3a575b8898d9da63a91f1397109e03
Julian Jones <julianj96@yahoo.com>
2023-04-12 17:06:02 -0500

    authentication model

--- frontend/src/components/TheWelcome.vue
+++ frontend/src/components/TheWelcome.vue
@@ -81,9 +81,10 @@ import SupportIcon from './icons/IconSupport.vue'
       </template>
       <template #heading>Support Vue</template>
 
-      As an independent project, Vue relies on community backing for its sustainability. You can help
-      us by
-      <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+      <div>
+        As an independent project, Vue relies on community backing for its sustainability. You can help us by
+        <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+      </div>
     </WelcomeItem>
   </div>
 </template>
