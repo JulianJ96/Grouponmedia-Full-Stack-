d21a3e6709fd5ebe274c0379bcacefe717387e66
Julian Jones <julianj96@yahoo.com>
2023-04-12 18:00:50 -0500

    setting up components

--- frontend/src/auth/store.js
+++ frontend/src/auth/store.js
@@ -1,1 +1,53 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+
+export default createStore({
+  state() {
+    return {
+      user: {
+        status: '',
+        token: '',
+        user: '',
+        id: '',
+        idComment: '',
+        firstname: '',
+        lastname: '',
+        post_unread: '',
+      },
+    };
+  },
+  mutations: {
+    login(state, user) {
+      localStorage.setItem('userId', user.data.email);
+      localStorage.setItem('token', user.data.token);
+      localStorage.setItem('id', user.data.userId);
+      state.user.token = user.data.token;
+      state.user.user = user.data.email;
+      state.user.id = user.data.userId;
+      state.user.firstname = user.data.firstname;
+      state.user.lastname = user.data.lastname;
+    },
+    unread(state, unread) {
+      localStorage.setItem('unread', unread);
+      state.user.post_unread = unread;
+    },
+    comment(state, comment) {
+      localStorage.setItem('idComment', comment);
+      state.user.idComment = comment;
+    },
+    clear(state) {
+      state.user.user = '';
+      state.user.token = '';
+      state.user.id = '';
+      state.user.idComment = '';
+      state.user.firstname = '';
+      state.user.lastname = '';
+      state.user.post_unread = '';
+    },
+  },
+  actions: {},
+  getters: {
+    getUser: (state) => {
+      return state.user.user;
+    },
+  },
+});
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="main">
+    <h1>{{ msg }}</h1>
+    <main class="form-signin">
+      <img class="mb-4 img-fluid" src="../assets/images/icon-left-font-monochrome-white.svg" alt="Grupomania">
+      <form>
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <div class="form-floating">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="username" required>
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="password" required>
+          <label for="floatingPassword">Password</label>
+        </div>
+        <div class="checkbox mb-3">
+          <button class="w-100 btn btn-lg btn-primary" type="submit" @click="SignInUser">Sign In</button>
+        </div>
+        <div class="checkbox mb-3">
+          <button class="w-100 btn btn-lg btn-primary" type="submit"  @click="SignUpUser">Sign Up</button>
+        </div>
+        <p class="mt-5 mb-3 alert-danger" id="response" ></p>
+        <p class="mt-5 mb-3 text-muted">&copy; 2020–2021</p>
+      </form>
+    </main>
+  </div>
+</template>
+
+<script>
+//import axios from "axios";
+//import { AUTH_REQUEST } from "../auth/actions/auth";
+import { mapState } from "vuex";
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  props: {
+    msg: String
+  },
+  computed: {
+    ...mapState({
+      user: (state) => state.user
+    })
+  },
+  methods: {
+    SignUpUser(event) { 
+      event.preventDefault()
+      this.$router.push('/signup')
+    },
+    SignInUser(event) {
+      event.preventDefault() 
+      let answer = document.getElementById("response"); 
+      if (this.username != "" && this.password != "") {
+        let url = "http://localhost:3000/api/auth/login"
+        let data1 = {
+          email : this.username,
+          password : this.password,
+        }
+        this.$http.post(url,data1).then(response => {
+          this.$store.commit('login',response); 
+          this.$router.push('/dashboard')
+        }).catch(error => {
+          answer.innerHTML =  error.response.data.message;
+        });
+      }else{
+        answer.innerHTML =  "Please fill the fields in order to sign in";
+      }
+    },
+  }
+}
+</script>
