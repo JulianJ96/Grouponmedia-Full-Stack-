d1901423dbbfbc7ef8c584648d7e9b9f038a451c
Julian Jones <julianj96@yahoo.com>
2023-04-12 18:17:39 -0500

    finished login

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -32,7 +32,7 @@
 import { mapState } from "vuex";
 
 export default {
-  name: 'Login',
+  name: 'SaveUser',
   data() {
     return {
       username: "",
@@ -74,3 +74,66 @@ export default {
   }
 }
 </script>
+
+
+<style scoped lang="scss">
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  html,
+  body {
+    height: 100%;
+  }
+  body {
+    display: flex;
+    align-items: center;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background-color: #f5f5f5;
+  }
+  .form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+  }
+  .form-signin .checkbox {
+    font-weight: 400;
+  }
+  .form-signin .form-floating:focus-within {
+    z-index: 2;
+  }
+  .form-signin input[type="email"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+  .form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
+  .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+  }
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
+</style>
