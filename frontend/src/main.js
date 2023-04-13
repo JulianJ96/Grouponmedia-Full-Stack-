bab256ca126462117faea873064d8defe70c96e4
Julian Jones <julianj96@yahoo.com>
2023-04-12 23:56:14 -0500

    component pages done (change errors later)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,11 +6,20 @@ import global from './assets/main.css';
 const base = axios.create({
     baseURL: "http://localhost:3000/api"
 });
+import VueAxios from 'vue-axios'
+import VueTelInput from 'vue3-tel-input'
+import 'vue3-tel-input/dist/vue3-tel-input.css'
+import Vuelidate from 'vuelidate'
+import "boostrap/dist/css/boostrap.min.css"
+import "boostrap"
 const app = createApp(App)
-
 app.use(router)
 app.use(global)
 app.config.globalProperties.$http = base;
+app.use(VueAxios, axios)
+app.use(Vuelidate)
+app.use(VueTelInput);
+app.config.compilerOptions.isCustomElement = tag => tag.startsWith('ion-')
 app.mount('#app')
 
 
