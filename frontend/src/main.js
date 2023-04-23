6167ee5ac251289226c923ac2cd3ce7e36fb3dbc
Julian Jones <julianj96@yahoo.com>
2023-04-23 11:27:25 -0500

    finishing up signup.vue component page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,6 +10,7 @@ import VueAxios from 'vue-axios'
 import VueTelInput from 'vue3-tel-input'
 import 'vue3-tel-input/dist/vue3-tel-input.css'
 import Vuelidate from 'vuelidate'
+import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 const app = createApp(App)
 app.use(router)
