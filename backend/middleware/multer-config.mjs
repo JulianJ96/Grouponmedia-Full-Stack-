7248b6ae0a828a55b2b18fa87ac81c05fa335fec
Julian Jones <julianj96@yahoo.com>
2023-06-27 18:04:39 -0500

    switched images multer to front end. now they work

--- backend/middleware/multer-config.mjs
+++ backend/middleware/multer-config.mjs
@@ -1,5 +1,9 @@
 import multer from 'multer';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -10,15 +14,17 @@ const MIME_TYPES = {
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    const destinationPath = path.join(__dirname, '..', 'assets');
+    const destinationPath = path.join(__dirname, '..', '..', 'frontend', 'src', 'assets');
     callback(null, destinationPath);
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    callback(null, `${name}${Date.now()}.${extension}`);
   }
 });
 
-export default multer({ storage: storage }).single('files');
+export default multer({ storage }).single('files');
+
+
 
